200e9c0743077345b74d154c33ba4ed4da5eb474
Gyeltshen gempo <gyeltshen.gempo@dcpl.bt>
2022-06-09 05:55:15 +0600

    header

--- src/components/Auth/SignUp/SignUpSelection.js
+++ src/components/Auth/SignUp/SignUpSelection.js
@@ -5,6 +5,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import {Card} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
+import Typography from '@mui/material/Typography';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -22,21 +23,23 @@ export default function SignUpSelection({handleAction, open}) {
         onClose={handleAction}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>{'Select Registration type'}</DialogTitle>
-        <DialogContent style={{width: 400}}>
-          <Card style={{padding: 20, textAlign: 'center', marginBottom: 10, cursor: 'pointer'}}
+        <DialogTitle sx={{backgroundColor: '#D6D6D6', color: "#2f2f2f"}}>
+          <Typography align={"center"} fontSize={20}>Select Registration type</Typography>
+        </DialogTitle>
+        <DialogContent sx={{width: 500, backgroundColor: '#d6d6d6'}}>
+          <Card className={'card-shadow'} style={{padding: 20, textAlign: 'center', marginBottom: 10, cursor: 'pointer'}}
                 onClick={() => navigate('/sign-up?as=agent')}>
             Travel Agent
           </Card>
-          <Card style={{padding: 20, textAlign: 'center', marginBottom: 10, cursor: 'pointer'}}
+          <Card className={'card-shadow'} style={{padding: 20, textAlign: 'center', marginBottom: 10, cursor: 'pointer'}}
                 onClick={() => navigate('/sign-up?as=guide')}>
             Tour Guide
           </Card>
-          <Card style={{padding: 20, textAlign: 'center', marginBottom: 10, cursor: 'pointer'}}
+          <Card className={'card-shadow'} style={{padding: 20, textAlign: 'center', marginBottom: 10, cursor: 'pointer'}}
                 onClick={() => navigate('/sign-up?as=hiring')}>
-            Car / Bus hiring agency
+            Car / Bus Hiring Agency
           </Card>
-          <Card style={{padding: 20, textAlign: 'center', cursor: 'pointer'}}
+          <Card className={'card-shadow'} style={{padding: 20, textAlign: 'center', cursor: 'pointer'}}
                 onClick={() => navigate('/sign-up?as=hotel')}>
             Hotel Management
           </Card>
--- src/view/Landing/Banner/Banner.js
+++ src/view/Landing/Banner/Banner.js
@@ -9,16 +9,20 @@ function Banner() {
     <Stack sx={{position: 'relative'}}>
       <img draggable={'false'} className='bannerImage card-shadow' src={Images.newBanner} alt="banner" />
       <Stack sx={{position: 'absolute', left: '8%', bottom: '15%'}}>
-        <Typography className={'noselect'} fontSize={32} fontWeight={700} color={'#fff'} sx={{mt: -1}}>
+        <Typography className={'noselect'} fontWeight={700} color={'#fff'}
+                    sx={{mt: -1, fontSize: {lg: 32, md: 32, sm: 32, xs: 20}}}>
           Striving towards achieving
         </Typography>
-        <Typography className={'noselect'} fontSize={48} fontWeight={700} color={'#fff'} sx={{mt: -1}}>
+        <Typography className={'noselect'} fontWeight={700} color={'#fff'}
+                    sx={{mt: -1, fontSize: {lg: 48, md: 48, sm: 48, xs: 25}}}>
           Vibrant and Sustainable
         </Typography>
-        <Typography className={'noselect'} fontSize={48} fontWeight={700} color={'#fff'} sx={{mt: -1}}>
+        <Typography className={'noselect'} fontWeight={700} color={'#fff'}
+                    sx={{mt: -1, fontSize: {lg: 48, md: 48, sm: 48, xs: 25}}}>
           Tourism Industry
         </Typography>
-        <Typography className={'noselect'} fontSize={32} fontWeight={700} color={'#fff'} sx={{mt: -1}}>
+        <Typography className={'noselect'} fontWeight={700} color={'#fff'}
+                    sx={{mt: -1, fontSize: {lg: 32, md: 32, sm: 32, xs: 15}}}>
           through connectedness
         </Typography>
       </Stack>
